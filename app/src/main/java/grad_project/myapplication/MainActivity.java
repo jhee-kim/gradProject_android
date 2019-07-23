b28e8c0b4c209f84db02e800fed25ff8af148b84
jh98765 <jhk7951@gmail.com>
2019-07-23 19:56:02 +0900

    테스트

--- app/src/main/java/grad_project/myapplication/MainActivity.java
+++ app/src/main/java/grad_project/myapplication/MainActivity.java
@@ -28,6 +28,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+// 깃 추가 테스트!
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences infoData;
     boolean is_login = false;
