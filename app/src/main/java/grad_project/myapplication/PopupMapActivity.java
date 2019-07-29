16cd48827667034dc75d45ede3d15fda87a0bd21
kim5712 <jaeheonkim212@gmail.com>
2019-07-29 21:39:14 +0900

    Merge branch 'map2' of C:\Users\a0105\AndroidStudioProjects\android2 with conflicts.

---
+++ app/src/main/java/grad_project/myapplication/PopupMapActivity.java
@@ -0,0 +1,27 @@
+package grad_project.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class PopupMapActivity extends Activity {
+
+/*
+    @Override
+    protected void onCreate(@androidx.annotation.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+
+        setContentView(R.layout.popup_map);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)width, (int)(height * 0.25));
+    }*/
+}
