c703652bfe7b74f122222f1275635456f49cdb5e
Wjeongyeon <woo9515jy@koreatech.ac.kr>
2019-07-30 19:34:09 +0900

    Map2에 QR코드 추가(시험)

--- app/src/main/java/grad_project/myapplication/ConfirmActivity.java
+++ app/src/main/java/grad_project/myapplication/ConfirmActivity.java
@@ -1,18 +1,19 @@
 package grad_project.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class ConfirmActivity extends AppCompatActivity {
-
+    private Button btr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
-
         ActionBar actionBar = getSupportActionBar();
         if (actionBar == null) {
             throw new NullPointerException("Null ActionBar");
--- app/src/main/java/grad_project/myapplication/PopupMapActivity.java
+++ app/src/main/java/grad_project/myapplication/PopupMapActivity.java
@@ -2,8 +2,10 @@ package grad_project.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -11,18 +13,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 public class PopupMapActivity extends Activity {
 
     ImageView MuseImage;
     TextView MuseTitle, MuseSub;
     String Mus_Title, Mus_Sub;
     Button QR_Button;
+    private SharedPreferences infoData;
+    private IntentIntegrator qrScan;
+    private String[] qrArr = {"0", "1", "2", "3", "4", "5"};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popup_map);
+        infoData = getSharedPreferences("infoData", MODE_PRIVATE);
+        qrScan = new IntentIntegrator(this);
+        //loadInfo();   infoData 값들 불러오기
 
         Intent intent = getIntent();
 
@@ -89,6 +100,58 @@ public class PopupMapActivity extends Activity {
 
     //QR을 위한 액티비티 연결 필요
     public void QRClick(View view){
-        Toast.makeText(getApplicationContext(), "QR연결 요구", Toast.LENGTH_SHORT).show();
+        qrScan.setBeepEnabled(false);
+        qrScan.setPrompt("전시관 QR코드를 스캔해주세요.");
+        qrScan.initiateScan();
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // QR코드/ 바코드를 스캔한 결과
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        // result.getFormatName() : 바코드 종류
+        // result.getContents() : 바코드 값
+        String qrUrl = result.getContents();
+
+        if (qrUrl == null) {
+            Toast.makeText(getApplicationContext(), "내용이 존재하지 않습니다.", Toast.LENGTH_LONG).show();
+        } else { //QR코드, 내용 존재
+            try {
+                int i_exhibitionNum = findCorrespondExhibition(qrUrl);
+                if(i_exhibitionNum >= 1 && i_exhibitionNum <= 6) {
+                    Toast.makeText(getApplicationContext(), i_exhibitionNum + "전시관의 QR코드와 일치합니다!", Toast.LENGTH_LONG).show();
+                    //infoData에 찍힌 QR코드 정보 저장하기
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "QR코드가 일치하지 않습니다.", Toast.LENGTH_LONG).show();
+                }
+                Log.v("qrcode Contents : ", qrUrl);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.v("Exception : ", "QR code fail");
+            }
+        }
+    }
+    /*
+    private void loadInfo() {   //QR코드 URL들 불러오기 + 배열로 만들기
+        s_name = infoData.getString("NAME", "");
+        s_number = infoData.getString("NUMBER", "");
+    }
+    }
+    */
+
+    public int findCorrespondExhibition(String qrUrl) {
+        int resultExhibition = -1;
+        for(int i = 0 ; i < 6 ; i++) {   //URL 비교(전체 전시관 or 해당 전시관에 대해)
+            if(qrUrl.equals(qrArr)) {
+                resultExhibition = i;
+                break;
+            }
+        }
+        //결과반환
+        return resultExhibition;
+    }
+
 }
