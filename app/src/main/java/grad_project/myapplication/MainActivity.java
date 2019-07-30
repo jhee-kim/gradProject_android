4f43b47f0db45677ae10a03492801b0e18b91629
jh98765 <jhk7951@gmail.com>
2019-07-30 20:55:29 +0900

    코드 정리

--- app/src/main/java/grad_project/myapplication/MainActivity.java
+++ app/src/main/java/grad_project/myapplication/MainActivity.java
@@ -96,19 +96,6 @@ public class MainActivity extends AppCompatActivity {
             actionBar.hide();
         }
 
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo("kr.ac.koreatech.cse.test1", PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
         // 메인 화면 버튼
         bt_openMap = findViewById(R.id.bt_open_map);
         bt_registration = findViewById(R.id.bt_registration);
