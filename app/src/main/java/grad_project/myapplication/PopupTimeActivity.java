4ea9220e2f1aeb3c5129b7b32005bf07f8c4f4e8
Handonggon <gksehdrhs2@gmail.com>
2019-08-02 00:53:13 +0900

    MapPopup 디자인 수정

--- app/src/main/java/grad_project/myapplication/PopupTimeActivity.java
+++ app/src/main/java/grad_project/myapplication/PopupTimeActivity.java
@@ -22,7 +22,9 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class PopupTimeActivity extends Activity {
     private Chronometer mChronometer;
@@ -35,17 +37,16 @@ public class PopupTimeActivity extends Activity {
         startDate = intent.getLongExtra("Time", 0);
 
         setContentView(R.layout.popup_time);
-        //mChronometer.setTextColor(Color.RED);
-
         mChronometer = (Chronometer)findViewById(R.id.chronometer);
+
         //time 초기화
         mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener(){
             @Override
             public void onChronometerTick(Chronometer cArg) {
                 long now = System.currentTimeMillis() - 9*60*60*1000;
                 long time = now - startDate;
-                //if(time >= 2*60*60*1000) {mChronometer.setTextColor(Color.RED);}
-                cArg.setText(DateFormat.format("관람시간 kk:mm:ss", time));
+                //if() {mChronometer.setTextColor(Color.parseColor("#FF0000"));}
+                cArg.setText(DateFormat.format("kk:mm:ss", time));
             }
         });
         mChronometer.start();
@@ -56,7 +57,7 @@ public class PopupTimeActivity extends Activity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
         getWindow().setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
-        getWindow().setLayout((int)(width*0.95), (int)(height * 0.2));
+        getWindow().setLayout((int)(width*0.8), (int)(height * 0.2));
     }
     @Override
     protected void onPause() {
