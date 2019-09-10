db88fd739560b4765ea2e57549e6ad86356c4392
Handonggon <gksehdrhs2@gmail.com>
2019-09-10 21:33:08 +0900

    list view 마지

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -13,3 +13,14 @@ Java_grad_1project_myapplication_OcrActivity_ConvertRGBtoGray(JNIEnv *env, jobje
 
     cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
 }
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_grad_1project_myapplication_CompareActivity_ConvertRGBtoGray(JNIEnv *env, jobject thiz,
+                                                                  jlong matAddrInput,
+                                                                  jlong matAddrResult) {
+    Mat &matInput = *(Mat *)matAddrInput;
+    Mat &matResult = *(Mat *)matAddrResult;
+
+    cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
+}
