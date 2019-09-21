ade392ce61113eec9d711699629dfc49897080e5
jh98765 <jhk7951@gmail.com>
2019-09-21 16:20:44 +0900

    관람방법 도움말 버튼 웹페이지로 연결

--- app/src/main/java/grad_project/myapplication/RegistActivity.java
+++ app/src/main/java/grad_project/myapplication/RegistActivity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
@@ -273,7 +274,9 @@ public class RegistActivity extends AppCompatActivity {
         bt_partHelp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "//관람 방법에 대한 설명 띄우기//", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(), "//관람 방법에 대한 설명 띄우기//", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.i815.or.kr/2018/tour/armyVisit.do"));
+                startActivity(intent);
             }
         });
 
